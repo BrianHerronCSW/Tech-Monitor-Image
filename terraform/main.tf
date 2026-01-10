430c53b59acd3365d70a3ffc09d9116203c32903
BrianHerronCSW <brian.herron@capstoneworks.com>
2026-01-10 00:54:54 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,7 +123,7 @@ resource "azurerm_virtual_network_gateway_connection" "CSW_VPN_Connection" {
   virtual_network_gateway_id     = azurerm_virtual_network_gateway.CSW_VPNGW.id
   local_network_gateway_id       = azurerm_local_network_gateway.CSW_OnPrem_LNG.id
   type                = "IPsec"
-  routing_weight                 = 10
+  routing_weight                 = 9
   shared_key                     = var.VPN_SHARED_KEY
 
   ipsec_policy {
