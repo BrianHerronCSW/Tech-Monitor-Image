d7785d85f5ee2a20538cd0611fd44936185eb5a0
BrianHerronCSW <brian.herron@capstoneworks.com>
2026-01-10 14:38:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1364,8 +1364,10 @@ def main():
   if os.getenv("DEBUG_MODE") == "true":
     app.run(host='0.0.0.0', port=5000, debug=True)
 
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+main()
+
 if __name__ == "__main__":
-  gunicorn_logger = logging.getLogger('gunicorn.error')
-  app.logger.handlers = gunicorn_logger.handlers
-  app.logger.setLevel(gunicorn_logger.level)
-  main()
+  pass
